ed59770491e862304ff989373867c4554797411b
sunghj1118 <sunghj1118@gmail.com>
2023-11-21 09:45:52 +0900

    [FEAT] add baseline code to fetch realtime subway info

---
+++ openseoul-subway-api/realtime.py
@@ -0,0 +1,36 @@
+import os
+import requests
+import datetime
+
+user_key = '7a56424b6173756e333449616b4563'
+station = '신촌'
+url = f'http://swopenAPI.seoul.go.kr/api/subway/{user_key}/json/realtimeStationArrival/0/30/{station}'
+
+response = requests.get(url)
+
+# Get the directory of the current script
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+# Define the new directory
+new_dir = 'json_responses/realtime'
+
+# Join the directory with the new directory
+new_dir_path = os.path.join(dir_path, new_dir)
+
+# Check if the new directory exists, if not, create it
+if not os.path.exists(new_dir_path):
+    os.makedirs(new_dir_path)
+
+# Get the current time
+now = datetime.datetime.now()
+
+# Format the current time into a string
+time_str = now.strftime('%Y%m%d_%H%M%S')
+
+# Join the new directory with the filename
+file_path = os.path.join(new_dir_path, f'realtime_{station}-{time_str}.json')
+
+# Save to new file
+with open(file_path, 'wb') as file:
+    # Write the content of the response to the file
+    file.write(response.content)
