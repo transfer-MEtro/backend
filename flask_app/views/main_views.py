56a1a682e0dc1ae7c175a1c887e6ffaa7e1d214e
sunghj1118 <sunghj1118@gmail.com>
2023-11-26 06:29:24 +0900

    [FEAT] add live subway data to db for Sinchon

--- flask_app/views/main_views.py
+++ flask_app/views/main_views.py
@@ -1,12 +1,45 @@
 from flask import Blueprint, jsonify
 import pymysql
+import realtime_arrival_list
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-def init_db():
+def insert_data():
+    # Insert data into the table
     db = pymysql.connect(host='127.0.0.1', port=3306, user='root',
                          passwd='root123!', db='metro_db', charset='utf8')
+    cursor = db.cursor()
+
+    # Get the realtime arrival list
+    arrival_list = realtime_arrival_list.extract_json()
+
+    insert_sql_template = '''INSERT INTO metro_db.Subway
+                (btrainNo, subwayId, statnFid, statnTid, statnId, statnNm, barvlDt)
+                VALUES (%s, %s, %s, %s, %s, %s, %s);'''
+
+    for item in arrival_list:
+        # Extract variables from each dictionary in the list
+        btrainNo = item['btrainNo']
+        subwayId = item['subwayId']
+        statnFid = item['statnFid']
+        statnTid = item['statnTid']
+        statnId = item['statnId']
+        statnNm = item['statnNm']
+        barvlDt = item['barvlDt']
+
+        # Print the SQL statement
+        cursor.execute(insert_sql_template, (btrainNo, subwayId,
+                       statnFid, statnTid, statnId, statnNm, barvlDt))
+        # Commit the transaction
+        db.commit()
+    db.close()
+    return f"Number of rows inserted: {cursor.rowcount}"
+
+
+def init_db():
+    db = pymysql.connect(host='127.0.0.1', port=3306, user='root',
+                         passwd='root123!', db='metro_db', charset='utf8mb4')
     cursor = db.cursor()
     db_sql = '''CREATE DATABASE IF NOT EXISTS metro_db default CHARACTER SET UTF8;'''
     table_sql = '''CREATE TABLE IF NOT EXISTS metro_db.Subway
@@ -16,9 +49,9 @@ def init_db():
                 statnFid INT NOT NULL,
                 statnTid INT NOT NULL,
                 statnId INT NOT NULL,
-                statnNm VARCHAR(32) NOT NULL,
+                statnNm VARCHAR(100) CHARACTER SET utf8mb4,
                 barvlDt INT NOT NULL
-            ) ENGINE = INNODB;'''
+            ) ENGINE = INNODB DEFAULT CHARSET=utf8mb4;'''
     cursor.execute(db_sql)
     cursor.execute(table_sql)
     result = cursor.fetchall()
@@ -37,9 +70,10 @@ def hello_flask():
     return 'Hello, Flask!'
 
 
-@bp.route('/bye')
+@bp.route('/insert')
 def bye_flask():
-    return 'Bye, Flask!'
+    insert = insert_data()
+    return insert
 
 
 @bp.route('/json_test')
