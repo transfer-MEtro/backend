41b02c0818beedabbe34533b04fcb15ae579344e
sunghj1118 <sunghj1118@gmail.com>
2023-12-21 20:34:52 +0900

    [FEAT] update on parameters

--- flask_app/realtime_arrival_list.py
+++ flask_app/realtime_arrival_list.py
@@ -6,10 +6,9 @@ line_2 = ['시청', '을지로입구', '을지로3가', '을지로4가', '동대
           '사당', '낙성대', '서울대입구', '봉천', '신림', '신대방', '구로디지털단지', '대림', '신도림', '문래', '영등포구청', '당산', '합정', '홍대입구', '신촌', '이대', '아현', '충정로', '용답', '신답', '용두', '신설동', '도림천', '양천구청', '신정네거리', '까치산']
 
 
-def extract_json():
+def extract_json(station):
     # Get key here: https://data.seoul.go.kr/dataList/OA-12764/F/1/datasetView.do
     user_key = '7a56424b6173756e333449616b4563'
-    station = '신촌'
     url = f'http://swopenAPI.seoul.go.kr/api/subway/{user_key}/json/realtimeStationArrival/0/30/{station}'
 
     response = requests.get(url)
