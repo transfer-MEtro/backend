93ff0134ccb3f28c5436c4fb6ccfe19fc80258f7
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-12-22 10:41:58 +0900

    :sparkles: Replace database caching to real time crawling

--- flask_app/views/main_views.py
+++ flask_app/views/main_views.py
@@ -21,48 +21,64 @@ def get_lines():
     return jsonify(get_station_by_line())
 
 
-def _get_station(station: str):
-    with get_db().cursor() as cursor:
-        sql = '''SELECT btrainNo, subwayId, statnFid, statnTid, statnId, statnNm, barvlDt FROM metro_db.Subway WHERE statnNm = %s;'''
-        cursor.execute(sql, (station,))
-        result = cursor.fetchall()
+def _get_arrival(station: str) -> list[dict]:
+    result_list = []
 
-        # Convert the result into a list of dictionaries
-        result_list = []
-        for item in result:
-            train_id = item[0]
-            subway_id = item[1]
+    # arrivals = []
+    # with get_db().cursor() as cursor:
+    #     sql = '''SELECT btrainNo, subwayId, statnFid, statnTid, statnId, statnNm, barvlDt FROM metro_db.Subway WHERE statnNm = %s;'''
+    #     cursor.execute(sql, (station,))
+    #     arrivals = cursor.fetchall()
 
-            if str(subway_id)[2] != '0':
-                continue
+    # for item in arrivals:
+    #     train_id = item[0]
+    #     subway_id = item[1]
+    #     previous_station_id = item[2]
+    #     next_station_id = item[3]
+    #     station_id = item[4]
+    #     station_name = item[5]
+    #     estimated_time_arrival = item[6]
 
-            line_number = str(subway_id)[3]
+    arrivals = RealtimeArrival().get_realtime_arrival_by_station(station)
 
-            # Convert the tuple into a dictionary
-            item_dict = {
-                'trainId': train_id,
-                'subwayId': subway_id,
-                'previousStationId': item[2],
-                'nextStationId': item[3],
-                'stationId': item[4],
-                'stationName': item[5],
-                'estimatedTimeArrival': item[6],
-                'lineNumber': line_number,
-            }
-            result_list.append(item_dict)
+    for item in arrivals:
+        train_id = item['btrainNo']
+        subway_id = item['subwayId']
+        previous_station_id = item['statnFid']
+        next_station_id = item['statnTid']
+        station_id = item['statnId']
+        station_name = item['statnNm']
+        estimated_time_arrival = item['barvlDt']
 
-        return result_list
+        if str(subway_id)[2] != '0':
+            continue
+
+        line_number = str(subway_id)[3]
+
+        item_dict = {
+            'trainId': train_id,
+            'subwayId': subway_id,
+            'previousStationId': previous_station_id,
+            'nextStationId': next_station_id,
+            'stationId': station_id,
+            'stationName': station_name,
+            'estimatedTimeArrival': estimated_time_arrival,
+            'lineNumber': line_number,
+        }
+        result_list.append(item_dict)
+
+    return result_list
 
 
 @bp.route('/stations/<station>')
-def get_station(station: str):
-    result_list = _get_station(station)
+def get_arrival(station: str):
+    result_list = _get_arrival(station)
     return jsonify(result_list)
 
 
 @bp.route('/congestions/<station>')
-def get_realtime_arrival_by_station(station: str):
-    result_list = _get_station(station)
+def list_congestions_by_station(station: str):
+    result_list = _get_arrival(station)
     for item in result_list:
         line_number = item['lineNumber']
         train_id = item['trainId']
@@ -113,11 +129,6 @@ def insert_data(station):
             # Commit the transaction
             cursor.connection.commit()
         return f"Number of rows inserted: {cursor.rowcount}"
-
-
-@bp.route('/insert')
-def insert():
-    return insert_data("신촌")
 
 
 @bp.route('/insert/<line>')
